217f37937b33c69afa9ee57f6e83af2869392e84
GrumpyCrouton <12193403+GrumpyCrouton@users.noreply.github.com>
2024-04-07 12:04:08 -0500

    Localization support
    Fixes #10

--- Assets/_Scripts/ProductWorldCanvas.cs
+++ Assets/_Scripts/ProductWorldCanvas.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Localization.Components;
 
 public class ProductWorldCanvas : MonoBehaviour
 {
@@ -9,12 +10,27 @@ public class ProductWorldCanvas : MonoBehaviour
     [SerializeField] private TextMeshProUGUI typeValueText;
     [SerializeField] private TextMeshProUGUI sizeValueText;
 
+    private LocalizeStringEvent localizedNameString;
+    private LocalizeStringEvent localizedTypeString;
+    private LocalizeStringEvent localizedSizeString;
+
     public bool CanShow = true;
 
     public void Setup(ProductSO productSO) {
-        nameValueText.text = productSO.ProductName;
-        typeValueText.text = productSO.productType.ToString();
-        sizeValueText.text = productSO.productSize.ToString();
+        if(localizedSizeString == null)
+        {
+            localizedNameString = nameValueText.GetComponent<LocalizeStringEvent>();
+            localizedTypeString = typeValueText.GetComponent<LocalizeStringEvent>();
+            localizedSizeString = sizeValueText.GetComponent<LocalizeStringEvent>();
+        }
+
+        localizedNameString.StringReference.TableEntryReference = productSO.LocalizationKey;
+        localizedTypeString.StringReference.TableEntryReference = $"Type{productSO.productType}";
+        localizedSizeString.StringReference.TableEntryReference = $"Size{productSO.productSize}";
+
+        nameValueText.text = localizedNameString.StringReference.GetLocalizedString();
+        typeValueText.text = localizedTypeString.StringReference.GetLocalizedString();
+        sizeValueText.text = localizedSizeString.StringReference.GetLocalizedString();
     }
 
     public void Show() {
